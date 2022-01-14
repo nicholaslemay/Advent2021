e63508785603cd53a1e6e34e36f7753036c102bb
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 14:41:06 -0500

    Bingo game can be won both by row and column

--- spec/Day04/part01_spec.rb
+++ spec/Day04/part01_spec.rb
@@ -10,7 +10,7 @@ class BingoGame
     drawn_numbers = []
     @number_sequence.each do |number|
       drawn_numbers << number
-      winning_board = @boards.find{|board| has_winning_row?(board, drawn_numbers)}
+      winning_board = @boards.find{|board| has_winning_row?(board, drawn_numbers) || has_winning_column?(board, drawn_numbers)}
       return score_from(winning_board, drawn_numbers) * number if winning_board
     end
   end
@@ -18,13 +18,21 @@ class BingoGame
   private
 
   def has_winning_row?(board, drawn_numbers)
-    board.any?{|row| row.all?{|x| drawn_numbers.include?(x)}}
+    board.any?{|row| row.all?{|n| has_been_drawn?(n, drawn_numbers)}}
+  end
+
+
+  def has_winning_column?(board, drawn_numbers)
+    board.transpose.any?{|column| column.all?{|n| has_been_drawn?(n, drawn_numbers)}}
   end
 
   def score_from(winning_board, drawn_numbers)
-    winning_board.flatten.reject{|x| drawn_numbers.include?(x)}.sum
+    winning_board.flatten.reject{|n| has_been_drawn?(n, drawn_numbers)}.sum
   end
 
+  def has_been_drawn?(n, drawn_numbers)
+    drawn_numbers.include?(n)
+  end
 end
 
 RSpec.describe BingoGame do
@@ -37,6 +45,16 @@ RSpec.describe BingoGame do
         [[66,77],[22,44]]
       ])
       expect(bingo_game_new.score).to be(44 * (66 + 77))
+    end
+  end
+
+  describe "a winning board by column" do
+    it "scores is the last number drawn * the sum of winning's board undrawn numbers" do
+      bingo_game_new = BingoGame.new([7, 8, 77], [
+        [[1,2],[3,4]],
+        [[7,2],[8,4]],
+      ])
+      expect(bingo_game_new.score).to be(8 * (2 + 4))
     end
   end
 
