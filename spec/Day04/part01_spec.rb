7de25fc5a1604a0fe50984061ca6bf164a307c81
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 14:27:26 -0500

    Bingo game score is based on last drawn number

--- spec/Day04/part01_spec.rb
+++ spec/Day04/part01_spec.rb
@@ -1,32 +1,38 @@
 require_relative '../spec_helper'
 
 class BingoGame
-  def initialize(drawnNumbers, boards)
-    @drawnNumbers = drawnNumbers
+  def initialize(number_sequence, boards)
+    @number_sequence = number_sequence
     @boards = boards
   end
 
   def score
-    @boards.find_index{|board| has_winning_row?(board)}
+    drawn_numbers = []
+    @number_sequence.each do |number|
+      drawn_numbers << number
+      winning_index = @boards.find_index{|board| has_winning_row?(board, drawn_numbers)}
+      return winning_index * number if winning_index
+    end
+
   end
 
   private
 
-  def has_winning_row?(board)
-    board.any?{|row| row.all?{|x| @drawnNumbers.include?(x)}}
+  def has_winning_row?(board, drawn_numbers)
+    board.any?{|row| row.all?{|x| drawn_numbers.include?(x)}}
   end
 
 end
 
 RSpec.describe BingoGame do
 
-  it "score is the index of the winning board when won by row" do
-    bingo_game_new = BingoGame.new([22, 44], [
+  it "score is the index times the last number of the winning board when won by row" do
+    bingo_game_new = BingoGame.new([22, 44, 77], [
       [[1,2],[3,4]],
       [[1,2],[3,4]],
       [[66,77],[22,44]]
     ])
-    expect(bingo_game_new.score).to be(2)
+    expect(bingo_game_new.score).to be(2 * 44)
   end
 
 end
