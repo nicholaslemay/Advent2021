7036fee018c2655de6bc231b89e9b891645a30a3
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 14:09:30 -0500

    Bingo game returns winning board index for boards containing a single element

--- spec/Day04/part01_spec.rb
+++ spec/Day04/part01_spec.rb
@@ -1,14 +1,20 @@
 require_relative '../spec_helper'
 
 class BingoGame
+  def initialize(drawnNumber, boards)
+    @drawnNumber = drawnNumber
+    @boards = boards
+  end
+
   def score
+    @boards.find_index{|x| x == @drawnNumber}
   end
 end
 
 RSpec.describe BingoGame do
 
-  it "returns a bogus score by default" do
-    expect(BingoGame.new.score).to be(nil)
+  it "score is the index of the winning board" do
+    expect(BingoGame.new(22, [33,44,22]).score).to be(2)
   end
 
 end
