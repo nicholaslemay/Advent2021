3161837da3b546ac0f4c9e2cd0191e0f80d68c38
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 21:12:43 -0500

    Submarine part 01: Moving forward increases horizontal position

--- spec/Day02/part01_spec.rb
+++ spec/Day02/part01_spec.rb
@@ -4,11 +4,24 @@ class Submarine
   def initialize
     self.horizontal_position = 0
   end
+
+  def forward(increase)
+    self.horizontal_position += increase
+  end
 end
 
 RSpec.describe "Part 01" do
+  let(:sub) { Submarine.new }
+
   it "has an initial horizontal position of zero" do
-    sub = Submarine.new
     expect(sub.horizontal_position).to eq(0)
+  end
+
+  it "increases horizontal position on a forward move " do
+    sub.forward(5)
+    expect(sub.horizontal_position).to eq(5)
+
+    sub.forward(8)
+    expect(sub.horizontal_position).to eq(13)
   end
 end
