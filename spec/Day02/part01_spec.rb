47f924beea1019cad2e5167daf7743d282d5073f
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 21:09:42 -0500

    Submarine part 01: Submarine has default horizontal position

--- spec/Day02/part01_spec.rb
+++ spec/Day02/part01_spec.rb
@@ -1,9 +1,14 @@
 class Submarine
+  attr_accessor :horizontal_position
 
+  def initialize
+    self.horizontal_position = 0
+  end
 end
 
 RSpec.describe "Part 01" do
-  it "works" do
-    Submarine
+  it "has an initial horizontal position of zero" do
+    sub = Submarine.new
+    expect(sub.horizontal_position).to eq(0)
   end
 end
