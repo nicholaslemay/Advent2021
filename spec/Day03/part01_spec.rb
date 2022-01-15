b17855b3b552d75920b07d932c1d70cf9d1e5cd5
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 22:29:32 -0500

    Power consumption part 01: Epsilon is the inverse of the gamma rate

--- spec/Day03/part01_spec.rb
+++ spec/Day03/part01_spec.rb
@@ -1,14 +1,28 @@
 
 class PowerCalculator
-  def self.gamma_rate(diagnostic)
-    diagnostic.to_i(2)
+  def self.gamma_rate(diagnostic_entries)
+    most_frequent_bits = ''
+    bits_grouped_together = diagnostic_entries.map(&:chars).transpose
+    bits_grouped_together.select do |bits|
+      most_frequent_bits += bits.max_by { |i| bits.count(i) }
+    end
+    most_frequent_bits.to_i(2)
+  end
+
+  def self.epsilon_rate(diagnostic_entries)
+    self.gamma_rate(diagnostic_entries) ^ '11111'.to_i(2)
   end
 end
 
 RSpec.describe "Something" do
 
-  it "gamma rate is the decimal value of the bits of the diagnostic" do
-    diagnostic = '000010'
-    expect(PowerCalculator.gamma_rate(diagnostic)).to eq(2)
+  it "gamma rate is the decimal value of the most frequents bits of each entry of the diagnostic" do
+    diagnostic = %w[00010 00011 00001]
+    expect(PowerCalculator.gamma_rate(diagnostic)).to eq(3)
+  end
+
+  it "epsilon rate is the decimal value of the most infrequent bits of each entry of the diagnostic" do
+    diagnostic = %w[10101]
+    expect(PowerCalculator.epsilon_rate(diagnostic)).to eq('01010'.to_i(2))
   end
 end
