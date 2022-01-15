b69cd16634438f81f08e7d3019a849faeda845c2
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 22:01:54 -0500

    Power consumption part 01: PowerCalculator returns bogus gamma rate

--- spec/Day03/part01_spec.rb
+++ spec/Day03/part01_spec.rb
@@ -1,11 +1,12 @@
 
 class PowerCalculator
-
+  def self.gamma_rate
+    0
+  end
 end
 
-
 RSpec.describe "Something" do
-  it "works" do
-    PowerCalculator
+  it "has default gamma rate" do
+    expect(PowerCalculator.gamma_rate).to eq(0)
   end
 end
