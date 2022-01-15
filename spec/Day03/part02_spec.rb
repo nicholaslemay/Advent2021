f0d488096ae0aa5d552741f6835709a13ea494d2
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-15 15:24:06 -0500

    Life-support rating(part 02): Oxygen rating is a decimal value

--- spec/Day03/part02_spec.rb
+++ spec/Day03/part02_spec.rb
@@ -1,5 +1,16 @@
 class LifeSupportCalculator
   def self.oxygen_generator_rating(diagnostic_entries)
+    diagnostic_entries[0].to_i(2)
+  end
+end
+
+RSpec.describe LifeSupportCalculator do
+
+  describe 'oxygen rating' do
+    it 'returns the value of a single diagnostic in decimal directly' do
+      diagnostics = %w(1)
+      expect(LifeSupportCalculator.oxygen_generator_rating(diagnostics)).to be(1)
+    end
   end
 end
 
