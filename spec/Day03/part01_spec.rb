1dece6e140550276c7c344e7570ea70991d6bf0d
Nicholas Lemay <lemaynicholas@gmail.com>
2022-01-14 22:05:52 -0500

    Power consumption part 01: Game rate is the decimal value of the diagnostic

--- spec/Day03/part01_spec.rb
+++ spec/Day03/part01_spec.rb
@@ -1,12 +1,14 @@
 
 class PowerCalculator
-  def self.gamma_rate
-    0
+  def self.gamma_rate(diagnostic)
+    diagnostic.to_i(2)
   end
 end
 
 RSpec.describe "Something" do
-  it "has default gamma rate" do
-    expect(PowerCalculator.gamma_rate).to eq(0)
+
+  it "gamma rate is the decimal value of the bits of the diagnostic" do
+    diagnostic = '000010'
+    expect(PowerCalculator.gamma_rate(diagnostic)).to eq(2)
   end
 end
